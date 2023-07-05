60342e38254675811944d0c56dd901880904240d
Jancy <s.jancyjerin@gmail.com>
2023-07-05 16:58:12 -0500

    file modified

---
+++ src/app/employee-list/employee-list.component.ts
@@ -0,0 +1,50 @@
+import { ThrowStmt } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Employee } from '../employee'
+import { EmployeeService } from '../employee.service'
+@Component({
+  selector: 'app-employee-list',
+  templateUrl: './employee-list.component.html',
+  styleUrls: ['./employee-list.component.css']
+})
+export class EmployeeListComponent implements OnInit {
+
+  employees: Employee[];
+
+  constructor(private employeeService: EmployeeService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.getEmployees();
+  }
+
+  private getEmployees() {
+    console.log("inside getEmployees");
+    this.employeeService.getEmployeeList().subscribe(data => {
+      this.employees = data;
+    });
+
+
+    //this.employees = this.employeeService.getEmployeeList();
+  }
+
+
+  employeeDetails(id: number) {
+    this.router.navigate(['employee-details', id]);
+  }
+
+
+
+
+  updateEmployee(id: number) {
+    this.router.navigate(['update-employee', id]);
+
+  }
+
+  deleteEmployee(id: number) {
+    this.employeeService.deleteEmployee(id).subscribe(data => {
+      console.log(data);
+      this.getEmployees();
+    })
+  }
+}
